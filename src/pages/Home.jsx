76db93b4d88092a686176de2f8f0084cdce70142
AntoineMHB <antoinemuhumbania@gmail.com>
2024-08-17 02:17:49 +0200

    Front end with the header and the sidebar done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+const Home = () => {
+  return (
+    <div>
+         {/* THE DASHBOARD TEXT */}
+     <div className="ml-[78px] mt-[35px]">
+        <h1 className='text-4xl font-extrabold text-customBlack font-poppins'>Dashboard</h1>
+        <p className='text-xl font-medium text-customDarkGray font-poppins'>welcome back, Antoine</p>
+     {/* but the name should be coming from the username of the user */}
+
+     </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        "purple": "#5A57FF",
+        "light-white": "rgba(255,255,255,0.17)",
+        customGray: '#C2C0FF',
+        customWhite: '#FFFFFF',
+        customHover: '#4A47E0',
+        customBlack: '#353535',
+        customDarkGray: '#CCCCCC',
+        sortGray: '#D9D9D9',
+        notificationPink: '#FF5787',
+      }
+    },
+  },
+  plugins: [],
+}
+
